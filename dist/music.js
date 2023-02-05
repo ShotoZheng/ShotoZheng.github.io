57c1e04ef9c6ced3290ba0a8df0e0ad02e552467
ShotoZheng <shotozheng@163.com>
2023-02-05 17:27:27 +0800

    Site updated: 2023-02-05 17:26:07

---
+++ dist/music.js
@@ -0,0 +1,37 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "十年水流东，十年水流西",
+        artist: '五条人',
+        url: 'http://music.163.com/song/media/outer/url?id=28587843.mp3',
+        cover: 'https://p2.music.126.net/whoMNaWcVMrIinwTnlQYyA==/109951165923601720.jpg?param=90y90',
+      },
+      {
+        name: "画",
+        artist: '赵雷',
+        url: 'http://music.163.com/song/media/outer/url?id=202369.mp3',
+        cover: 'https://p1.music.126.net/wldFtES1Cjnbqr5bjlqQbg==/18876415625841069.jpg?param=90y90',
+      },
+      {
+        name: "千千阙歌",
+        artist: '陈慧娴',
+        url: 'http://music.163.com/song/media/outer/url?id=212233.mp3',
+        cover: 'https://p1.music.126.net/tzmGFZ0-DPOulXS97H5rmA==/18712588395102549.jpg?param=90y90',
+      },
+      {
+        name: "游山恋",
+        artist: '夏艺韩',
+        url: 'http://music.163.com/song/media/outer/url?id=1471055851.mp3',
+        cover: 'https://p2.music.126.net/9JoqniYrEXjcK0z5J6W5Zg==/109951165234718088.jpg?param=90y90',
+      },
+      {
+        name: 'memories',
+        artist: '17years after～',
+        url: 'http://music.163.com/song/media/outer/url?id=424477092.mp3',
+        cover: 'http://p2.music.126.net/groyO2EWS-7RDm7Al90Zjw==/3417282149451064.jpg?param=130y130',
+      }
+    ]
+});
